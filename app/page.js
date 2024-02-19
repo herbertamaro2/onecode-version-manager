74213bfa299ccd6bb092208ae17dbd6596ceb0fa
herbertamaro2 <herbert.webdesigner@gmail.com>
2024-02-18 21:39:44 -0300

    Add everything

--- app/page.js
+++ app/page.js
@@ -1,113 +1,173 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react";
+import { FaArrowLeft, FaRegFileCode } from "react-icons/fa";
+import { MdOutlineTaskAlt } from "react-icons/md";
+import updates from './updates.json'
+import procedures from './procedures.json'
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
 
 export default function Home() {
+  const [filesFrom, setFilesFrom] = useState(null);
+  const [fileProcedure, setFileProcedure] = useState('');
+  const lastCommit = 'https://gitlab.com/geraldofcastro/jupiter/-/commit/54a85792031e18cb681b957fa82e62c3d910f1fd';
+  const stableCommit = 'https://gitlab.com/geraldofcastro/jupiter/-/commit/54a85792031e18cb681b957fa82e62c3d910f1fd';
+  const [openLightbox, setOpenLightbox] = useState(false);
+  const [image, setImage] = useState('');
+
+
+  const handleImage = (image) => {
+    setImage(image);
+    setOpenLightbox(true);
+  }
+
+  const handleCloseLightbox = () => {
+    setImage('');
+    setOpenLightbox(false);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
+
+    <main className="flex min-h-screen flex-col relative pt-5 w-11/12 mx-auto">
+      <div className="w-full flex items-center justify-between pb-10">
+        <img
+          src="/logo.png"
+          alt="Vercel Logo"
+          style={{ height: 60 }}
+        />
+        <h1 className='text-3xl font-bold'>Gerenciamento de Versão</h1>
+      </div>
+      <div className="w-full flex flex-row text-center justify-between bg-gray-100 rounded p-5">
+        <p className="text-xl flex flex-col items-start w-auto">
+          <span><strong>Estável:</strong> 2024.06.1286-beta</span>
+          <a href={stableCommit} target="_blank" className="text-md leading-5 text-blue-500">Commit</a>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <p className="text-xl flex flex-col items-end w-auto">
+          <span><strong>Última versão:</strong> 2024.06.1289-beta</span>
+          <a href={lastCommit} target="_blank" className="text-md leading-5 text-blue-500">Commit</a>
+        </p>
+        
+      </div>
+
+      <div className='w-full flex justify-between pt-10 pb-20 gap-5'>
+
+        <div className="w-4/12">
+          <h3 className='text-xl font-bold mb-4'>Atualizações</h3>
+
+          <ul area="list" className="divide-y divide-gray-100">
+            {updates && updates.map((update, index) => (
+              <li key={update.title} 
+              className={`list-li flex justify-between gap-x-6 py-5 ${filesFrom === String(index) && 'active'}`}
+              onClick={() => setFilesFrom(String(index))}>
+                <div className="flex min-w-0 gap-x-4">
+                  {update.imageUrl ? <img className="h-12 w-12 flex-none rounded-full bg-gray-50" 
+                  src={update.imageUrl} alt=""
+                  onClick={() => handleImage(update.imageUrl)} /> :
+                  <img  className="h-12 w-12 flex-none rounded-full bg-gray-50" src={'/noimage.png'} />
+                  }
+                  <div className="min-w-0 flex-auto">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">{update.title}</p>
+                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                      <div className="mt-1 flex items-center gap-x-1.5">
+                        {update.type === 'BUG' ?
+                          <div className="flex-none rounded-full bg-red-500/20 p-1">
+                            <div className="h-1.5 w-1.5 rounded-full bg-red-500" />
+                          </div>
+                          :
+                          <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                            <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                          </div>
+                        }
+                        <span className="text-xs leading-5 text-gray-500">{update.type}</span>
+                        {update.taskUrl && <a href={update.taskUrl} target="_blank"
+                        className="text-xs leading-5 text-blue-500">ABRIR TAREFA</a>}
+                      </div>
+                    </p>
+                  </div>
+                </div>
+                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                  <p className="text-sm leading-6 text-gray-900">{update.area}</p>
+                  <p className="text-xs leading-5 text-gray-500">{update.priority}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="w-4/12">
+          <h3 className='text-xl font-bold mb-4'>Arquivos alterados</h3>
+
+          {filesFrom ? (
+            <ul area="list" className="divide-y divide-gray-100">
+              {updates[Number(filesFrom)].affected.map((file, index) => (
+                <li key={index} 
+                className={`flex list-li justify-between gap-x-6 py-5  ${fileProcedure === file && 'active'}`}
+                onClick={() => setFileProcedure(file)}
+                >
+                  <div className="flex min-w-0 gap-x-4">
+                    <div className="min-w-0 flex  items-center gap-5 flex-auto" >
+                      <FaRegFileCode size={26} />
+                      <p className="text-sm font-semibold leading-6 text-gray-900">{file}</p>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>) : (
+            <div className='bg-gray-100 w-full h-full flex items-center justify-center rounded'>
+              <span className="flex gap-5 font-bold items-center justify-center"><FaArrowLeft /> Clique ao lado</span>
+            </div>
+          )}
+        </div>
+
+        <div className="w-4/12">
+          <h3 className='text-xl font-bold mb-4'>Procedimento de Testes</h3>
+          {fileProcedure ? (
+            <ul area="list" className="divide-y divide-gray-100">
+              {procedures.filter(item => item.file === fileProcedure).map((file, index) => (
+                file.tasks.map((task, index) => ( // Mapping over tasks
+                  <li key={index} className="flex list-li justify-between gap-x-6 py-5">
+                    <div className="flex min-w-0 gap-x-4">
+                      <div className="min-w-0 flex items-center gap-5 flex-auto">
+                        <MdOutlineTaskAlt size={26} />
+                        <p className="text-sm font-semibold leading-6 text-gray-900">{task}</p>
+                      </div>
+                    </div>
+                  </li>
+                ))
+              ))}
+            </ul>
+          ) : (
+            <div className='bg-gray-100 w-full h-full flex items-center justify-center rounded'>
+              <span className="flex gap-5 font-bold items-center justify-center"><FaArrowLeft /> Clique ao lado</span>
+            </div>
+          )}
+
+
+
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="w-full fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="https://onecode.com.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          Onecode Brasil{" "}
         </a>
       </div>
+
+      <Lightbox
+        open={openLightbox}
+        close={() => handleCloseLightbox()}
+        slides={[
+          { src: image }
+        ]}
+      />
     </main>
   );
 }
